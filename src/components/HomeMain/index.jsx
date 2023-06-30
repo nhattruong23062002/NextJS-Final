5f5f766019c1cfa8d248d086952e17ae7a65efa8
Nhat <Nhattr2306@gmail.com>
2023-06-30 23:43:01 +0700

    commit

--- src/components/HomeMain/index.jsx
+++ src/components/HomeMain/index.jsx
@@ -6,7 +6,7 @@ const HomeMain = () => {
       <div className="home_page ">
         <div className="home_img ">
           <img
-            src="https://scontent.fdad1-3.fna.fbcdn.net/v/t39.30808-6/316423004_5712106928880733_3437443915088622002_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=e3f864&_nc_ohc=I24GUOFWSxEAX_FtHA-&_nc_ht=scontent.fdad1-3.fna&oh=00_AfAjIkVGDW_Mvm3264_zt5pBoNEqFBN9l5ojcf6w5gAtnQ&oe=649737BA"
+            src="https://marketplace.canva.com/EAE9CdfAHDw/1/0/1600w/canva-red-black-minimalist-promotion-special-black-friday-banner-4iNCPcVUZtY.jpg"
             alt="img "
           />
         </div>
